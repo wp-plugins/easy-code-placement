9a6f02c0463ff075b9cff5d619602d820ad8f8b8
wassereimer <wassereimer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 18:22:42 +0000

    new version for tags
    git-svn-id: https://plugins.svn.wordpress.org/easy-code-placement/tags/2.5@1089260 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/actions/ecp_delete.php
@@ -0,0 +1,23 @@
+<?php
+
+global $wpdb;
+
+// secure get data and set variables
+$_GET = stripslashes_deep($_GET);
+$ecp_id = intval($_GET['ecpid']);
+
+if($ecp_id=="" || !is_numeric($ecp_id)) {
+    // when get emty or other than numbers goto error page
+    $ecp_error = __('Modifying of the ID is not allowed', 'ecp');
+    $ecp_error_page = "";
+    $ecp_error_id = "";
+    return(ecp_error($ecp_error, $ecp_error_page, $ecp_error_id));
+}
+
+// delete code
+$wpdb->delete($wpdb->prefix.'ecp_data', array('id'=>$ecp_id));
+
+// when data is deleted goto options page	
+header('Location: options-general.php?page=ecp_option_page');
+
+?>
